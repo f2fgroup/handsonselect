fb8a335406c44165374a0a2baab6ce491c33e9d2
Ioan CHIRIAC <ichiriac@f2f.fr>
2018-04-20 12:01:18 +0200

    fix validator

--- src/validator.js
+++ src/validator.js
@@ -15,15 +15,26 @@
           if (typeof value === "string") {
             value = value.trim();
           }
+          // scan each entry of the datasource
           for(var i = 0; i <  this.options.data.length; i++) {
             if (this.options.data[i] == value || this.options.data[i].id == value) {
+              // found by id
               return callback(true);
             } else if (this.options.data[i].text == value) {
+              // found by text
               return callback(true);
             }
           }
+          // did not found
+          return callback(false);
+        } else {
+          // the value is empty
+          if (this.options.mandatory) {
+            callback(false);
+          } else {
+            callback(true);
+          }
         }
-        callback(false);
       } else {
         callback(true);
       }
